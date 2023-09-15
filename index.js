cecee11fe7c7f2d99e83d52f7dff3beec50a6cf9
Edward <edwardtoro688@gmail.com>
2023-09-14 21:17:11 -0300

    se agregaron nuevas carpetas y archivos

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { taskRouter } from "./src/routes/task.routes.js";
 import { startDb } from "./src/config/database.js";
+import cors from 'cors'
+import morgan from 'morgan';
+import helmet from 'helmet'
 
 const app = express();
+
 app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+app.use(helmet({
+    contentSecurityPolicy: false
+}))
 
 const port = 3000;
 
